ab37427462d3cf37509163f95b4cdb5fcc591cc0
ab4ilas4 <abhilashsgajare@gmail.com>
2019-10-24 23:02:48 +0530

    UserList Assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'user-list',
+  pathMatch: 'full'
+},
+{
+  path: 'user-list',
+  loadChildren:'./user-list/user-list.module#UserListModule'},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
